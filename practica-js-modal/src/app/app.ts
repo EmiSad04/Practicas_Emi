03f42808f57a91e2732f0d461d1c78e28bee6381
EmiSad04 <uppemi24@micorreo.upp.edu.mx>
2026-02-12 14:46:36 -0600

    cambios

--- practica-js-modal/src/app/app.ts
+++ practica-js-modal/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactoComponet } from './contacto-componet/contacto-componet';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContactoComponet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
