ed4dc9adc070a9935a2841d8a6a29f19b5150984
EmiSad04 <uppemi24@micorreo.upp.edu.mx>
2026-02-12 14:42:24 -0600

    cambios

---
+++ practica-js-modal/src/app/contacto-componet/contacto-componet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-componet',
+  imports: [],
+  templateUrl: './contacto-componet.html',
+  styleUrl: './contacto-componet.css',
+})
+export class ContactoComponet {
+
+}
